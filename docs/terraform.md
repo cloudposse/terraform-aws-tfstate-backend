766e5c1f97a7fb03ca8fa3976df1210d2b8a64f8
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-11 13:42:19 +0300

    Migrate readme yaml (#6)

---
+++ docs/terraform.md
@@ -0,0 +1,29 @@
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| acl | The canned ACL to apply to the S3 bucket | string | `private` | no |
+| attributes | Additional attributes (e.g. `state`) | list | `<list>` | no |
+| delimiter | Delimiter to be used between `namespace`, `stage`, `name`, and `attributes` | string | `-` | no |
+| enable_server_side_encryption | Enable DynamoDB server-side encryption | string | `true` | no |
+| force_destroy | A boolean that indicates the S3 bucket can be destroyed even if it contains objects. These objects are not recoverable | string | `false` | no |
+| name | Name  (e.g. `app` or `cluster`) | string | `terraform` | no |
+| namespace | Namespace (e.g. `cp` or `cloudposse`) | string | - | yes |
+| read_capacity | DynamoDB read capacity units | string | `5` | no |
+| region | AWS Region the S3 bucket should reside in | string | - | yes |
+| stage | Stage (e.g. `prod`, `dev`, `staging`) | string | - | yes |
+| tags | Additional tags (e.g. map(`BusinessUnit`,`XYZ`) | map | `<map>` | no |
+| write_capacity | DynamoDB write capacity units | string | `5` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| dynamodb_table_arn | DynamoDB table ARN |
+| dynamodb_table_id | DynamoDB table ID |
+| dynamodb_table_name | DynamoDB table name |
+| s3_bucket_arn | S3 bucket ARN |
+| s3_bucket_domain_name | S3 bucket domain name |
+| s3_bucket_id | S3 bucket ID |
+
--- output.tf
+++ output.tf
@@ -1,23 +1,29 @@
 output "s3_bucket_domain_name" {
-  value = "${aws_s3_bucket.default.bucket_domain_name}"
+  value       = "${aws_s3_bucket.default.bucket_domain_name}"
+  description = "S3 bucket domain name"
 }
 
 output "s3_bucket_id" {
-  value = "${aws_s3_bucket.default.id}"
+  value       = "${aws_s3_bucket.default.id}"
+  description = "S3 bucket ID"
 }
 
 output "s3_bucket_arn" {
-  value = "${aws_s3_bucket.default.arn}"
+  value       = "${aws_s3_bucket.default.arn}"
+  description = "S3 bucket ARN"
 }
 
 output "dynamodb_table_name" {
-  value = "${element(coalescelist(aws_dynamodb_table.with_server_side_encryption.*.name, aws_dynamodb_table.without_server_side_encryption.*.name), 0)}"
+  value       = "${element(coalescelist(aws_dynamodb_table.with_server_side_encryption.*.name, aws_dynamodb_table.without_server_side_encryption.*.name), 0)}"
+  description = "DynamoDB table name"
 }
 
 output "dynamodb_table_id" {
-  value = "${element(coalescelist(aws_dynamodb_table.with_server_side_encryption.*.id, aws_dynamodb_table.without_server_side_encryption.*.id), 0)}"
+  value       = "${element(coalescelist(aws_dynamodb_table.with_server_side_encryption.*.id, aws_dynamodb_table.without_server_side_encryption.*.id), 0)}"
+  description = "DynamoDB table ID"
 }
 
 output "dynamodb_table_arn" {
-  value = "${element(coalescelist(aws_dynamodb_table.with_server_side_encryption.*.arn, aws_dynamodb_table.without_server_side_encryption.*.arn), 0)}"
+  value       = "${element(coalescelist(aws_dynamodb_table.with_server_side_encryption.*.arn, aws_dynamodb_table.without_server_side_encryption.*.arn), 0)}"
+  description = "DynamoDB table ARN"
 }
