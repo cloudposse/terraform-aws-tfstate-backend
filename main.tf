dc1d2833d6a7e5df9e82a7c15c2ae2031623a4f1
lafarer <eric.lafargue@gmail.com>
2019-02-20 20:17:51 +0100

    Configure bucket public access setting

--- main.tf
+++ main.tf
@@ -39,6 +39,15 @@ resource "aws_s3_bucket" "default" {
   tags = "${module.s3_bucket_label.tags}"
 }
 
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket                  = "${aws_s3_bucket.default.id}"
+
+  block_public_acls       = "${var.block_public_acls}"
+  ignore_public_acls      = "${var.ignore_public_acls}"
+  block_public_policy     = "${var.block_public_policy}"
+  restrict_public_buckets = "${var.restrict_public_buckets}"
+}
+
 module "dynamodb_table_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.3"
   context    = "${module.base_label.context}"
