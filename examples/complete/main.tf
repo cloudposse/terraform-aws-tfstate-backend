fea415e5d93f28169693893000b057e76b34d2f0
aknysh <andriy.knysh@gmail.com>
2019-06-21 16:21:27 -0400

    Convert to TF 0.12. Add Tests. Add Codefresh test pipeline

---
+++ examples/complete/main.tf
@@ -0,0 +1,12 @@
+module "tfstate_backend" {
+  source = "../../"
+
+  providers = {
+    aws = "aws"
+  }
+
+  region    = var.region
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.name
+}
---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 0.12.0"
+}
+
+# Pin the `aws` provider
+# https://www.terraform.io/docs/configuration/providers.html
+# Any non-beta version >= 2.0.0 and < 3.0.0, e.g. 2.X.Y
+provider "aws" {
+  version = "~> 2.0"
+}
+
+provider "null" {
+  version = "~> 2.0"
+}
