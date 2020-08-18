db8e76ab04d7664aeb5f72ea5ac093a0447e7e53
Matt Gowie <matt@masterpoint.io>
2020-08-18 15:39:00 -0600

    Updates local output file to have normal file permissions - not 777
    Terraform's local_file creates permissions with 777 by default, which is odd. Using 644 is more sane and should keep file permissions in line with most system new file defaults.

--- main.tf
+++ main.tf
@@ -248,7 +248,8 @@ data "template_file" "terraform_backend_config" {
 }
 
 resource "local_file" "terraform_backend_config" {
-  count    = var.terraform_backend_config_file_path != "" ? 1 : 0
-  content  = data.template_file.terraform_backend_config.rendered
-  filename = local.terraform_backend_config_file
+  count           = var.terraform_backend_config_file_path != "" ? 1 : 0
+  content         = data.template_file.terraform_backend_config.rendered
+  filename        = local.terraform_backend_config_file
+  file_permission = "0644"
 }
