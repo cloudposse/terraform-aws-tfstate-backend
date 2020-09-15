838a72dc89da83bc2be72887be9bf0bd4e3af60f
Jeff <woz5999@gmail.com>
2020-09-15 14:39:04 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
+  required_version = ">= 0.12.0"
 
   required_providers {
-    aws      = "~> 2.0"
-    template = "~> 2.0"
-    local    = "~> 1.2"
-    null     = "~> 2.0"
+    aws      = ">= 2.0"
+    template = ">= 2.0"
+    local    = ">= 1.2"
+    null     = ">= 2.0"
   }
 }
