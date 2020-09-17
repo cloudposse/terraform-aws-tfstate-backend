bacd68aca1cda00036b50ae502d8c86e5a4cd658
Dan Meyers <20687081+danjbh@users.noreply.github.com>
2020-09-16 19:25:00 -0700

    Revert recent dynamodb changes (#69)

--- variables.tf
+++ variables.tf
@@ -139,12 +139,6 @@ variable "enable_public_access_block" {
   default     = true
 }
 
-variable "enable_dynamodb" {
-  type        = bool
-  description = "Enable DynamoDB Locking"
-  default     = true
-}
-
 variable "block_public_acls" {
   type        = bool
   description = "Whether Amazon S3 should block public ACLs for this bucket"
