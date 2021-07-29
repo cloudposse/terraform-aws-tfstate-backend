a60e0e1b75481caf9eb53874105948b6d49da85f
nitrocode <nitrocode@users.noreply.github.com>
2021-07-29 19:29:29 -0400

    Downgrade null label back to 0.22.0

--- main.tf
+++ main.tf
@@ -167,8 +167,8 @@ resource "aws_s3_bucket_public_access_block" "default" {
 
 module "dynamodb_table_label" {
   source     = "cloudposse/label/null"
-  version    = "0.24.1"
-  attributes = compact(concat(["lock"], var.attributes))
+  version    = "0.22.0"
+  attributes = compact(concat(var.attributes, ["lock"]))
   context    = module.this.context
 }
 
