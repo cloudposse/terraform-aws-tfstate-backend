3320bf49886f9a3406407c513ce5e23534c4d970
V Anastassiou <vicky.anastassiou@slalom.com>
2022-08-03 11:48:48 -0700

    Fix inaccurate variable description

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "force_destroy" {
 
 variable "mfa_delete" {
   type        = string
-  description = "A boolean that indicates that versions of S3 objects can only be deleted with MFA. ( Terraform cannot apply changes of this value; https://github.com/terraform-providers/terraform-provider-aws/issues/629 )"
+  description = "Whether versions of S3 objects can only be deleted with MFA. (Terraform cannot apply changes of this value; https://github.com/terraform-providers/terraform-provider-aws/issues/629)"
   default     = "Disabled"
 }
 
