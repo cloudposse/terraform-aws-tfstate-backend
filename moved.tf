b415c784b2dc786a9e01d9eca1c57c4c3f5f017a
Nuru <Nuru@users.noreply.github.com>
2023-04-22 13:28:49 -0700

    Show how to revert to previous version in comments

--- moved.tf
+++ moved.tf
@@ -12,3 +12,8 @@ moved {
   from = aws_s3_bucket_public_access_block.default[0]
   to   = module.blue_bucket[0].aws_s3_bucket_public_access_block.each
 }
+
+# To revert, terraform state mv:
+# 'module.tfstate_backend.aws_dynamodb_table.locks[0]' 'module.tfstate_backend.aws_dynamodb_table.with_server_side_encryption[0]'
+# 'module.tfstate_backend.module.blue_bucket[0].aws_s3_bucket.each' 'module.tfstate_backend.aws_s3_bucket.default[0]'
+# 'module.tfstate_backend.module.blue_bucket[0].aws_s3_bucket_public_access_block.each' 'module.tfstate_backend.aws_s3_bucket_public_access_block.default[0]'
