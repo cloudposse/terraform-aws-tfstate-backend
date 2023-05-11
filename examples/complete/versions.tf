3fd0b275ddcbf72546b830fd31f03c3e6ae74854
Nuru <Nuru@users.noreply.github.com>
2023-05-11 00:00:04 -0700

    Fix provider versions

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = "= 4.9.0"
     }
     local = {
       source  = "hashicorp/local"
