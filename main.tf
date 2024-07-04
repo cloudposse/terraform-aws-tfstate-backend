01232bd6cef75e0bb408894152edf524c037a16b
Lennart Goedhart <lennart@omnibase.com.au>
2024-07-05 03:09:01 +1000

    Add ability to use KMS encryption (#178)
    Add the ability to use KMS encyption through the addition of the
    `sse_encryption` and `kms_master_key_id` variables.
    
    * Fixes https://github.com/cloudposse/terraform-aws-tfstate-backend/issues/105
    
    Co-authored-by: Lennart Goedhart <lgoedhart-ic@akiliinteractive.com>
    Co-authored-by: Matt Gowie <gowie.matt@gmail.com>

--- main.tf
+++ main.tf
@@ -203,7 +203,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = var.sse_encryption
+      kms_master_key_id = var.kms_master_key_id
     }
   }
 }
